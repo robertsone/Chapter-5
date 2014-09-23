17386a89a1a16ead8ca31f27601edb2dc629d528
robertsone <rufrrufr@yahoo.com>
2014-09-23 08:42:09 -0400

    FINISHED w/ dictionaries

---
+++ TeeBall/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeeBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] players;
+            System.Console.WriteLine("How many people are on the team?");
+            int length = Convert.ToInt32(System.Console.ReadLine()); 
+            players = new int[length, 6];
+            while (true)
+            {
+
+                System.Console.WriteLine("Please enter new player number to add update player info, or type 999 to exit");
+                int num = Convert.ToInt32(System.Console.ReadLine());
+                if (num == 999)
+                    break;
+                System.Console.WriteLine("How many bases did they run?");
+                int bases = Convert.ToInt32(System.Console.ReadLine());
+                if (bases==0)
+                    players[num,1] += 1;
+                else if (bases==1)
+                    players[num,2] += 1;
+                else if (bases==2)
+                    players[num,3] += 1;
+                else if (bases==3)
+                    players[num,4] += 1;
+                else if (bases==4)
+                    players[num,5] += 1;
+
+            }
+            System.Console.WriteLine("\n\n\n");
+            for (int number = 0; number < length;number++)
+            {
+                System.Console.WriteLine("{0,8}{1,8}{2,8}{3,8}{4,8}{5,8}", players[number, 0], players[number, 1], players[number, 2], players[number, 3], players[number, 4], players[number, 5]);
+            }
+
+      
+
+             
+        }
+    }
+}
--- WhippetBuss/Program.cs
+++ WhippetBuss/Program.cs
@@ -11,10 +11,21 @@ namespace WhippetBuss
         {
             System.Console.Write("How far did you go, in miles: ");
             int distance = Convert.ToInt32(System.Console.ReadLine());
-            int[] miles = { 99, 299, 499, 9999 };
-            int[] price = { 25, 40, 55, 70 };
-            for (int num=0;num,
+            int[] miles = { 99, 299, 499, 9999999, 999999999 };
+            int[] price = { 25, 40, 55, 70,70 };
+            for (int num = 0; num < miles.Length; num++)
+            {
+                if (miles[num] < distance && distance<miles[num+1])
+                {
+                    int cost = price[num+1];
+                    System.Console.Write(cost);
+                    break;
+                }
+            }
 
+            
         }
+
+        
     }
 }
